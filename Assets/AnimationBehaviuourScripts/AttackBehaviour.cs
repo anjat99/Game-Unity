ffa0319cfcd2be351070d3ee110e84e52689a367
Anja Tomić <72461048+anjat99@users.noreply.github.com>
2023-10-05 18:22:56 +0200

    Add Animation Behaviour Scripts

---
+++ Assets/AnimationBehaviuourScripts/AttackBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviour : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<Character>().Napad = true;
+
+        animator.SetFloat("brzina", 0);
+        if(animator.tag == "Player")
+        {
+            if (Player.Instance.NaTlu)
+            {
+                Player.Instance.Igrac.velocity = Vector2.zero;
+            }
+        }
+       
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //animator.GetComponent<Character>().Napad = false;
+        animator.GetComponent<Character>().Napad = false;
+        animator.GetComponent<Character>().SwordCollider.enabled = false;
+
+        animator.ResetTrigger("napad");
+        animator.ResetTrigger("bacanje");
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
